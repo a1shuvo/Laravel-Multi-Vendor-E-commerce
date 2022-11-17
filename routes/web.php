fe7d14387095315c97341b3390271c703bd2babc
a1shuvo <a1shuvosaha@gmail.com>
2022-11-18 01:36:42 +0600

    Add Admin Middleware Authentication

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,10 @@ Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function
     // Admin Login Route
     Route::match(['get', 'post'], 'login', 'AdminController@login');
 
-    // Admin Dashboard Route
-    Route::get('dashboard', 'AdminController@dashboard');
+    Route::group(['middleware'=>['admin']],function(){
+        // Admin Dashboard Route
+        Route::get('dashboard', 'AdminController@dashboard');
+    });
+
 });
 
